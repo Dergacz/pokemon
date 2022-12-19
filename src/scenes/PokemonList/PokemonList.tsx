39bc488eb053e4daaeb1df60f46ec8606fd51cf7
Stas <sats96dergach@gamil.com>
2022-12-19 13:26:09 +0100

    fix code style

--- src/scenes/PokemonList/PokemonList.tsx
+++ src/scenes/PokemonList/PokemonList.tsx
@@ -3,11 +3,16 @@ import { pokemonApi } from '../../services/PokemonServiceApi';
 import { Pokemon } from '../Pokemon/Pokemon';
 
 export const PokemonList: FC = () => {
-  const { data: pokemons } = pokemonApi.useFetchAllPokemonQuery(100);
+  const { data: pokemons, isLoading } = pokemonApi.useFetchAllPokemonQuery(100);
   return (
     <div>
       {
-        pokemons && pokemons.results.map(pokemon => (
+        isLoading && (
+          <p>Loading...</p>
+        )
+      }
+      {
+        !isLoading && pokemons.results.map(pokemon => (
           <Pokemon
             key={pokemon.url}
             name={pokemon.name}
