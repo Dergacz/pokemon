3bfe0a29bb0667101a33c70700b834a8ef98ea89
Stas <sats96dergach@gamil.com>
2022-12-21 15:54:22 +0100

    add styles to components

--- src/scenes/MainPage/MainPage.tsx
+++ src/scenes/MainPage/MainPage.tsx
@@ -1,8 +1,22 @@
 import React, { FC } from 'react';
 import { PokemonList } from '../PokemonList/PokemonList';
+import Header from '../../components/Header/Header';
+import { Input, Pagination } from '@nextui-org/react';
 
 export const MainPage: FC = () => {
   return (
-    <PokemonList />
+    <>
+      <Header/>
+      <Input
+        className="main-page-search"
+        width="100%"
+        bordered
+        placeholder="search pokemon"
+      />
+      <PokemonList />
+      <footer className="main-page-pagination">
+        <Pagination total={20} initialPage={1} />
+      </footer>
+    </>
   );
 };
