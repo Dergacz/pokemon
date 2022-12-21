06cfe7612a07981d88d15a8bd1b07b362a557789
Stas <sats96dergach@gamil.com>
2022-12-21 16:00:12 +0100

    add brouser router

--- main.tsx
+++ main.tsx
@@ -3,10 +3,13 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './app';
 import { setupStore } from './src/store/store';
+import { BrowserRouter } from 'react-router-dom';
 
 const store = setupStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>, document.getElementById('root'));
